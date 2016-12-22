dd7758d7bdd3136666a5f9ba4b509e2359fa6157
GaoYanPeng <18842604977@163.com>
2016-12-22 17:36:40 +0800

    娱乐以一级界面

--- TVPanda/app/src/main/java/com/gaoyanpeng/tvpanda/live/LiveFm.java
+++ TVPanda/app/src/main/java/com/gaoyanpeng/tvpanda/live/LiveFm.java
@@ -1,14 +1,28 @@
 package com.gaoyanpeng.tvpanda.live;
 
 
+import android.graphics.Color;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.util.Log;
+
 import com.gaoyanpeng.tvpanda.R;
 import com.gaoyanpeng.tvpanda.base.BaseFragment;
+import com.gaoyanpeng.tvpanda.ok.NetTool;
+import com.gaoyanpeng.tvpanda.ok.onHttpCallBack;
+
+import okhttp3.OkHttpClient;
 
 /**娱乐赵宁来做 记住只能在自己的包下面做 别的包只能看不要改动
  * Created by 高研鹏 on 2016/12/17.
  */
 
 public class LiveFm extends BaseFragment {
+
+    private ViewPager mViewPager;
+    private TabLayout mTabLayout;
+    private LiveVPAdapter mLiveVPAdapter;
+
     @Override
     protected int initLayout() {
         return R.layout.fm_live;
@@ -16,11 +30,41 @@ public class LiveFm extends BaseFragment {
 
     @Override
     protected void initView() {
+        mViewPager = getFindView(R.id.live_vp);
+        mTabLayout = getFindView(R.id.live_tab);
+
+        mLiveVPAdapter = new LiveVPAdapter(getChildFragmentManager());
+        mViewPager.setAdapter(mLiveVPAdapter);
+        mTabLayout.setupWithViewPager(mViewPager);
+
+        mTabLayout.setTabTextColors(Color.rgb(0x81, 0x81, 0x81), Color.rgb(0x2E, 0xD2, 0x9C));
+        mTabLayout.setSelectedTabIndicatorColor(getResources().getColor(R.color.colorGrayLine));
 
     }
 
     @Override
     protected void initData() {
-
+        OkNetMethod();
     }
+
+//    网络请求方法
+    protected void OkNetMethod() {
+        NetTool.getInstance().startRequest(URLLive.LIIVEURL, LiveBean.class, new onHttpCallBack<LiveBean>() {
+            @Override
+            public void onSuccess(LiveBean response) {
+                Log.d("LiveFm", "网络请求成功");
+                Log.d("LiveFm", response.getData().getItems().get(0).getEnd_time());
+//                获取viewpager里面的标题
+                mLiveVPAdapter.setmBean(response);
+                mViewPager.setAdapter(mLiveVPAdapter);
+                mTabLayout.setupWithViewPager(mViewPager);
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.d("LiveFm", e.getMessage());
+            }
+        });
+    }
+
 }
--- TVPanda/app/src/main/java/com/gaoyanpeng/tvpanda/live/LiveVPAdapter.java
+++ TVPanda/app/src/main/java/com/gaoyanpeng/tvpanda/live/LiveVPAdapter.java
@@ -3,6 +3,7 @@ package com.gaoyanpeng.tvpanda.live;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
+import android.util.Log;
 
 /**
  * Created by Administrator on 2016/12/20.
@@ -10,20 +11,45 @@ import android.support.v4.app.FragmentStatePagerAdapter;
  */
 public class LiveVPAdapter extends FragmentStatePagerAdapter {
 
+    private static String [] Content = {"yzdr","hwzb","music","pets","boardgames","cartoon","sport","technology","finance"};
+    private static String [] title = {"熊猫星秀","户外直播","音乐","萌宠乐园","桌游","影评专区","体坛风云","科技前沿","金融理财"};
+
+    private static LiveBean mBean;
 
     public LiveVPAdapter(FragmentManager fm) {
         super(fm);
+    }
+
+    public  void setmBean(LiveBean mBean) {
+        LiveVPAdapter.mBean = mBean;
+        notifyDataSetChanged();
     }
 
 //    通过getmessage传值到对应的Fragment中返回newInstance方法
 //    其中newInstance方法是Fragment中一个静态常量方法
     @Override
     public Fragment getItem(int position) {
-        return ReuseFragment.newInstance(position);
+        return mBean!= null  &&mBean.getData().getItems().size() > 0 ?  ReuseFragment.newInstance(position) : null;
     }
 
+//    获取对应的返回页数
     @Override
     public int getCount() {
-        return 0;
+        return mBean == null ? 0 : title.length;
     }
+
+//    获取对应的标题
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return  title[position] ;
+    }
+
+//    获取字符串里面不同的total
+    public static String getMessage (int pos) {
+        return Content[pos];
+        /**
+         * afklalskjf
+         */
+    }
+
 }
